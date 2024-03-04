669d59d75b8f67b7631c16d22d1315bdb4c5ff08
salujan123 <161106744+salujan123@users.noreply.github.com>
2024-03-04 20:10:32 +0500

    Add files via upload

---
+++ guest_list.ts
@@ -0,0 +1,8 @@
+
+
+let guest_list :string[] = ['nawaz shareesf', 'asif zardari', 'imran khan'];
+
+for(let i=0; i<guest_list.length; i++){
+
+    console.log('Dear Mr. ' + guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank you!')
+}
